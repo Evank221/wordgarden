25412e9aa49d57bf274974a5e00f4c856b5a3345
Evank221 <evanharo49@gmail.com>
2026-01-12 10:40:35 -0800

    Initial Commit

---
+++ wordgarden/wordgardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  wordgardenApp.swift
+//  wordgarden
+//
+//  Created by HARO, EVAN on 1/12/26.
+//
+
+import SwiftUI
+
+@main
+struct wordgardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ wordgardenTests/wordgardenTests.swift
@@ -0,0 +1,17 @@
+//
+//  wordgardenTests.swift
+//  wordgardenTests
+//
+//  Created by HARO, EVAN on 1/12/26.
+//
+
+import Testing
+@testable import wordgarden
+
+struct wordgardenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
